75e475f90d3142e57a82f16cf4df2a46e7f2806c
Caiosouzadevv <caioss20.cs@gmail.com>
2025-10-20 16:56:03 -0300

    feat: new function searchCidade

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+const selectInput = document.querySelector('input[name="search-city"]');
+const btnSearch = document.querySelector('.site-header__btn-search');
+
+
+
+
+function searchCity(e) {
+    e.preventDefault();
+    btnSearch.addEventListener('click', function (e) {
+
+
+    });
+
+}
+
