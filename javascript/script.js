d530ba0a35f12939797adea8b57f9c2056d0de29
Caiosouzadevv <caioss20.cs@gmail.com>
2025-10-20 16:56:29 -0300

    feat: requisition fetch api

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,17 @@
 const selectInput = document.querySelector('input[name="search-city"]');
 const btnSearch = document.querySelector('.site-header__btn-search');
 
+async function loading() {
+  // Faz a requisição para a API
+  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m');
+
+  // Transforma o resultado da resposta em JSON (objeto JS)
+  const data = await response.json();
+
+  // Mostra no console pra gente ver o que veio
+  console.log(data);
+}
+
 
 
 
@@ -12,4 +23,5 @@ function searchCity(e) {
     });
 
 }
-
+// Chama a função
+loading();
