38faf63167296555a2d01e5054211c80240c3135
Caiosouzadevv <caioss20.cs@gmail.com>
2025-10-22 18:51:49 -0300

    feat: consynubg cuty api

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,8 @@
 const selectInput = document.querySelector('input[name="search-city"]');
 const btnSearch = document.querySelector('.site-header__btn-search');
 
-// Mapa de cidades com suas coordenadas
-const cities = {
-    berlin: { lat: 52.52, lon: 13.41 },
-    paris: { lat: 48.85, lon: 2.35 },
-    london: { lat: 51.51, lon: -0.13 },
-    rome: { lat: 41.90, lon: 12.49 },
-    madrid: { lat: 40.42, lon: -3.70 },
-    lisbon: { lat: 38.72, lon: -9.14 },
-    newyork: { lat: 40.71, lon: -74.01 },
-    tokyo: { lat: 35.68, lon: 139.69 },
-    sao_paulo: { lat: -23.55, lon: -46.63 },
-    rio_de_janeiro: { lat: -22.91, lon: -43.17 }
-};
+
+
 
 // Função de carregamento da API
 async function loading(lat, lon) {
@@ -26,6 +15,8 @@ async function loading(lat, lon) {
     console.log('Longitude:', lon);
     console.log('Temperatura:', data.current.temperature_2m, '°C');
     console.log('Vento:', data.current.wind_speed_10m, 'km/h');
+
+
 }
 
 // Função de busca
@@ -36,13 +27,42 @@ function searchCity() {
         const userCity = selectInput.value.toLowerCase().replace(/\s+/g, '_');
         console.log('Cidade digitada:', userCity);
 
-        if (cities[userCity]) {
-            const { lat, lon } = cities[userCity];
-            loading(lat, lon);
-        } else {
-            console.log('Cidade não encontrada no mapa.');
+
+        async function citiesFunction() {
+
+
+            const apiCities = `https://geocoding-api.open-meteo.com/v1/search?name=${userCity}`;
+
+            const response = await fetch(apiCities);
+            const data = await response.json();
+
+            data.results.forEach(e => {
+
+                if (userCity === e.name.toLowerCase().replace(/\s+/g, '_')) {
+                    let lat = e.latitude;
+                    let long = e.longitude;
+
+                    loading(lat, long);
+
+
+                    console.log(lat);
+
+
+                }
+
+
+
+            });
+
+
         }
+
+
+
+        citiesFunction();
+
     });
+
 }
 
 searchCity();
